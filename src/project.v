fc5aa15ff1d042585d1e9d6c82819147605e2f85
djuara <djuara@rbz.es>
2024-03-13 10:28:27 +0100

    Cambiado el edge del reset para tenerlo en el mismo flanco que el reloj sclk

--- src/project.v
+++ src/project.v
@@ -57,7 +57,7 @@ module tt_um_spi_test_djuara (
 
 	// Rising edge of SCLK, read commands (set MISO) and write commands (store data)
 	/*always @(posedge sclk, negedge rst_n, posedge cs) begin*/
-	always @(posedge sclk, negedge rst_n) begin
+	always @(posedge sclk, posedge rst_n) begin
 		if((rst_n == 0) || (cs == 1)) begin
 			spi_state 	<= Idle;
 			index 		<= 0;
